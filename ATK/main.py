92fee91a2203c483322359fa4f091080cbd5befd
Alexandre Dussolle <alexandredussolle@laposte.net>
2024-10-21 15:23:41 +0200

    window

--- ATK/main.py
+++ ATK/main.py
@@ -1,9 +1,16 @@
 """
-This module is the main module of the program, and can be executed to run the needed part of the project
+This module is the main module of the program,
+and can be executed to run the needed part of the project
 """
-
+from tkinter import Tk, Label
 
 # Entry point of the program
 if __name__ == "__main__":
     # create several players
     print("ok")
+    fenetre = Tk()
+
+    label = Label(fenetre, text="Hello World")
+    label.pack()
+
+    fenetre.mainloop()
