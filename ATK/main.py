aca73ad5e7804306c5675af80732abbfdec0deaa
Alexandre Dussolle <alexandredussolle@laposte.net>
2024-10-21 15:52:46 +0200

    descriptor

--- ATK/main.py
+++ ATK/main.py
@@ -3,10 +3,12 @@ This module is the main module of the program,
 and can be executed to run the needed part of the project
 """
 from tkinter import Tk, Label
+import Descriptor
 
 # Entry point of the program
 if __name__ == "__main__":
     print("ok")
+    Descriptor.display()
     fenetre = Tk()
 
     label = Label(fenetre, text="Hello World")
