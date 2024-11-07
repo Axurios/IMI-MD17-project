a2e34b85971bd8b6b427676ddd1885d0b9138944
Alexandre Dussolle <alexandredussolle@laposte.net>
2024-11-07 12:46:50 +0100

    data wrapper

--- ATK/DataWrapper.py
+++ ATK/DataWrapper.py
@@ -1,5 +1,8 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from typing import Tuple
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+# from typing import Tuple
 
+class Data:
+    def __init__(self):
+        self.tab = []
